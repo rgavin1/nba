87aa1e4dd2d987e8267fdb384af690022131aace
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-07-01 23:52:03 -0700

    Added Links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { Routes, Route, Link } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Clean up</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/players">Players</Link>
+        <Link to="/players/data">Players Data</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div><h1>HOME</h1></div>} />
+        <Route path="/search" element={<div><h1>Search</h1></div>} />
+        <Route path="/players" element={<div><h1>Player</h1></div>} />
+        <Route path="/players/data" element={<div><h1>Player Data</h1></div>} />
+      </Routes>
     </div>
   );
 }
